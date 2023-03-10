949e05905dfc4a99cfeabbd2ea1dc530b3e3d7a1
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-03-10 21:24:17 +0530

    created gig routes

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -19,18 +19,30 @@ export const createGig = async (req, res, next) => {
 };
 export const deleteGig = async (req, res, next) => {
   try {
+    const gig = await Gig.findById(req.params.id);
+
+    if (gig.userId !== req.userId)
+      return next(createError(403, "You can delete only your gig"));
+
+    await Gig.findByIdAndDelete(req.params.id);
+    res.status(200).send("Gig has been deleted!");
   } catch (err) {
     next(err);
   }
 };
 export const getGig = async (req, res, next) => {
   try {
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) next(createError(404, "Gig not found"));
+    res.status(200).send(gig);
   } catch (err) {
     next(err);
   }
 };
 export const getGigs = async (req, res, next) => {
   try {
+    const gigs = await Gig.find();
+    res.status(200).send(gigs);
   } catch (err) {
     next(err);
   }

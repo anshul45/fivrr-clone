71581470c921da51868dc99ba94311d72d78a315
anshul45 <anshulgarwal45@gmail.com>
2023-03-25 23:37:02 +0530

    create orders

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,9 +1,23 @@
 import createError from "../utils/createError";
+import Order from "../models/order.model,js"
+import Gig from "../models/gig.model.js"
 
 
 export const createOrder = async(req, res,next) => {
     try{ 
 
+        const gig = Gig.findById(req.params.gigId);
+        const newOrder = new Order({
+                gigId:gig._id,
+                img:gig.cover,
+                title:gig.title,
+                buyerId:req.userId,
+                sellerId:gig.userId,
+                price:gig.price,
+                payment_intent:"temporary"
+        });
+        await newOrder.save();
+            res.status(200).send("sucessfull");
     }
     catch(err){
 next(err);

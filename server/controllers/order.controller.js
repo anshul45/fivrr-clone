b79b6a059d8294aa31e7676a41ff18ca958144b5
anshul45 <anshulgarwal45@gmail.com>
2023-03-28 20:21:04 +0530

    conversation using node

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -24,4 +24,14 @@ next(err);
     }
 };
 
-export const getOrders = async (req,res,next)=>{}
+export const getOrders = async (req,res,next)=>{
+    try{
+        const orders = await Order.find({
+            ...(req.isSeller ? {sellerId:req.userId}:{buyerId:req.userId}),
+            isCompleted:true,
+        });
+        res.status(200).send(orders)
+    } catch(err){
+        next(err);
+    }
+}
--- server/routes/conversation.route.js
+++ server/routes/conversation.route.js
@@ -1,7 +1,10 @@
 import express from "express";
+import{getConversations,createConversation,getSingleConversation,updateConversation}from"../controllers/conversation.controller.js";
 
 const router = express.Router();
 
-router.get("/test");
-
+router.get("/",verifyToken,getConversations);
+router.get("/",verifyToken,createConversation);
+router.get("/single/:id",verifyToken,getSingleConversation);
+router.get("/:id",verifyToken,updateConversation);
 export default router;
